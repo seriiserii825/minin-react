cfcd5392bac47c4bfd98b5e8c467a8b2a4a77861
serii <seriiburduja@gmail.com>
2026-02-28 09:14:10 +0200

    feat: multi product

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
 import "./App.css";
+import Product from "./components/Product";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="flex w-screen flex-col h-screen items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
-      <h1 className="text-4xl font-bold text-white">Hello, World!</h1>
-      <p className="mt-4 text-lg text-white">Welcome to your React app with Tailwind CSS!</p>
-      <div className="mt-6 text-2xl font-semibold text-white">Count: {count}</div>
-      <button
-        className="mt-6 px-4 py-2 bg-white text-purple-500 rounded hover:bg-gray-200 transition"
-        onClick={() => setCount(count + 1)}>
-        Increment count
-      </button>
+    <div className="flex justify-center items-center w-screen  h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+      <div className="grid grid-cols-4 gap-2">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
     </div>
   );
 }
---
+++ src/components/Product.tsx
@@ -0,0 +1,7 @@
+export default function Product() {
+  return (
+    <div className="flex flex-col gap-2 mb-2 border border-gray-300 rounded-lg p-4 bg-white p-4">
+      <h2 className="text-xl font-bold">Product</h2>
+    </div>
+  );
+}
